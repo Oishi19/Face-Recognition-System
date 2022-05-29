5bc394656002f502ef05191fc02e7d5f805bf715
Oishi <oishibask@gmail.com>
2022-05-29 21:46:55 +0530

    Add files via upload

---
+++ camera_app.py
@@ -0,0 +1,134 @@
+from flask import Flask, render_template, Response, request
+import cv2
+import datetime, time
+import os, sys
+import numpy as np
+from threading import Thread
+
+
+global capture,rec_frame, switch, rec, out, name
+capture=0
+switch=1
+rec=0
+name= input("Enter your full name: ")
+
+#make directory to save pics
+try:
+    os.mkdir('D:/Face_Recognition/Train')
+except OSError as error:
+    pass
+
+#Load pretrained face detection model    
+net = cv2.dnn.readNetFromCaffe('./saved_model/deploy.prototxt.txt', './saved_model/res10_300x300_ssd_iter_140000.caffemodel')
+
+#instatiate flask app  
+app = Flask(__name__, template_folder='./templates')
+
+camera = cv2.VideoCapture(0)
+
+def record(out):
+    global rec_frame
+    while(rec):
+        time.sleep(0.05)
+        out.write(rec_frame)
+
+
+def detect_face(frame):
+    global net
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
+        (300, 300), (104.0, 177.0, 123.0))   
+    net.setInput(blob)
+    detections = net.forward()
+    confidence = detections[0, 0, 0, 2]
+
+    if confidence < 0.5:            
+            return frame           
+
+    box = detections[0, 0, 0, 3:7] * np.array([w, h, w, h])
+    (startX, startY, endX, endY) = box.astype("int")
+    try:
+        frame=frame[startY:endY, startX:endX]
+        (h, w) = frame.shape[:2]
+        r = 480 / float(h)
+        dim = ( int(w * r), 480)
+        frame=cv2.resize(frame,dim)
+    except Exception as e:
+        pass
+    return frame
+ 
+
+def gen_frames():  # generate frame by frame from camera
+    global out, capture,rec_frame
+    while True:
+        success, frame = camera.read() 
+        if success:
+            if(capture):
+                capture=0
+                p = os.path.sep.join(["{}.png".format(str(name).replace(":",''))])
+                cv2.imwrite(p, frame)
+            if(rec):
+                rec_frame=frame
+                frame= cv2.putText(cv2.flip(frame,1),"Recording...", (0,25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),4)
+                frame=cv2.flip(frame,1)
+
+            try:
+                ret, buffer = cv2.imencode('.jpg', cv2.flip(frame,1))
+                frame = buffer.tobytes()
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            except Exception as e:
+                pass
+        else:
+            pass
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/requests',methods=['POST','GET'])
+def tasks():
+    global switch,camera
+    if request.method == 'POST':
+        if request.form.get('click') == 'Capture':
+            global capture
+            capture=1
+        elif  request.form.get('stop') == 'Stop/Start':
+            
+            if(switch==1):
+                switch=0
+                camera.release()
+                cv2.destroyAllWindows()
+            else:
+                camera = cv2.VideoCapture(0)
+                switch=1
+
+        elif  request.form.get('rec') == 'Start/Stop Recording':
+            global rec, out
+            rec= not rec
+            if(rec):
+                now=datetime.datetime.now() 
+                fourcc = cv2.VideoWriter_fourcc(*'XVID')
+                out = cv2.VideoWriter('vid_{}.avi'.format(str(now).replace(":",'')), fourcc, 20.0, (640, 480))
+                #Start new thread for recording the video
+                thread = Thread(target = record, args=[out,])
+                thread.start()
+            elif(rec==False):
+                out.release()
+                          
+                 
+    elif request.method=='GET':
+        return render_template('index.html')
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
+    
+camera.release()
+cv2.destroyAllWindows()     
---
+++ face.py
@@ -0,0 +1,87 @@
+import cv2
+import numpy as np
+import face_recognition
+import streamlit as st
+import os
+from datetime import datetime
+import warnings
+warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning) 
+
+path = 'E:\\Face-Recognition-Attendance-Projects-main\\Train'
+images = []
+classNames = []
+encodelist = []
+myList = os.listdir(path)
+print(myList)
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+print(classNames)
+
+def findencodings(images):
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodelist.append(encode)
+    return encodelist
+
+def markattendance(name):
+    with open('D:\\Face_Recognition\\Attendance.csv', 'r+') as f:
+        mydatalist = f.readlines()
+
+        namelist = []
+        for line in mydatalist:
+            entry = line.split(',')
+            namelist.append(entry[0])
+            if name not in namelist:
+                now = datetime.now()
+                dtstring = now.strftime('%H:%M:%S')
+                d2 = now.strftime("%B %d, %Y")
+                f.writelines(f'\n{name},{d2},{dtstring}')
+
+#### For Screen capturing besides Webcam
+# def captureScreen(bbox=(300,300,690+300,530+300)):
+#     capScr = np.array(ImageGrab.grab(bbox))
+#     capScr = cv2.cvtColor(capScr, cv2.COLOR_RGB2BGR)
+#     return capScr
+
+encodeListKnown = findencodings(images)
+print('Encoding Complete')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+# img = captureScreen()
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+
+    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+# print(faceDis)
+        matchIndex = np.argmin(faceDis)
+
+        if faceDis[matchIndex] < 0.50:
+            name = classNames[matchIndex].upper()
+            markattendance(name)
+        else:
+            name = 'Unknown'
+        
+        # print(name)
+        y1, x2, y2, x1 = faceLoc
+        y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+        cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+
+    cv2.imshow('Webcam', img)
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
